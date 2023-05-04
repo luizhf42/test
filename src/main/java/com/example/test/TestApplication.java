47fd24eb28cdda5e3f0384ec67187d6ace696550
Luiz Henrique <luizhf42@gmail.com>
2023-05-04 16:38:29 -0300

    first commit

---
+++ src/main/java/com/example/test/TestApplication.java
@@ -0,0 +1,20 @@
+package com.example.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+
+    }
+
+    @GetMapping
+    public String getHello() {
+        return "Hello wirld";
+    }
+
+}
